938c95ecdb9705e4ace794d739e48d5270637ca9
Ibrahemtaha <overeemtaha@gmail.com>
2023-07-14 06:17:55 +0300

    1st Commit

---
+++ PatternPython/Right_Angle_Triangle_Shape.py
@@ -0,0 +1,12 @@
+#*
+#* *
+#* * *
+#* * * *
+#* * * * *
+
+#num = int(input("Enter Number of Rows: "))
+num = 5
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print()
