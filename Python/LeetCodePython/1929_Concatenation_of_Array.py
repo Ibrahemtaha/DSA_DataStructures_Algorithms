ca3845d4ed44aa941485eec21bf4e73944e34fc6
Ibrahemtaha <overeemtaha@gmail.com>
2023-07-16 14:02:51 +0300

    1929C

--- Python/LeetCodePython/1929_Concatenation_of_Array.py
+++ Python/LeetCodePython/1929_Concatenation_of_Array.py
@@ -26,6 +26,7 @@ class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums * 2
 
+# -----------------------------------------
 # Solution 2
 # class Solution:
 #     def getConcatenation(self, nums: List[int]) -> List[int]:
